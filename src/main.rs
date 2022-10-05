9be1fc7317fe367a950e3d30f1c03a2836c5fb79
Luca Casonato <hello@lcas.dev>
2022-10-05 10:57:29 +0200

    Create a JS runtime and execute the entrypoint
    Create a new deno_core::JsRuntime with default options and execute the
    entrypoint source code as a script.
    
    Also adds an example.js file to demonstrate the code is actually being
    executed.
    
    Run with `cargo run example.js`

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+use deno_core::JsRuntime;
+use deno_core::RuntimeOptions;
+
 fn main() {
     let path = std::env::args().nth(1).expect("entrypoint specified");
     let source_code = std::fs::read_to_string(&path).expect("entrypoint loaded");
 
-    println!("{}", source_code);
+    let mut runtime = JsRuntime::new(RuntimeOptions {
+        ..Default::default()
+    });
+    runtime
+        .execute_script(&path, &source_code)
+        .expect("execution succeeds");
 }
