86bd826975abde856264312b7075f1b1030c2a86
Luca Casonato <hello@lcas.dev>
2022-10-05 10:51:59 +0200

    Read the source code from disk
    The path to the entrypoint is specified in the first command line
    argument.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let path = std::env::args().nth(1).expect("entrypoint specified");
+    let source_code = std::fs::read_to_string(&path).expect("entrypoint loaded");
+
+    println!("{}", source_code);
 }
